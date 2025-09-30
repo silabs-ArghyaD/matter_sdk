e84260d4e15032f227b8ff84e2b1ce21569b65d3
ardutta <arghya.dutta@silabs.com>
2025-09-30 10:21:59 +0530

    Fix: Compile Error

--- src/app/tests/Test_CASESessionManager.cpp
+++ src/app/tests/Test_CASESessionManager.cpp
@@ -1,61 +1,85 @@
-#include <pw_unit_test/framework.h>
 #include <app/CASESessionManager.h>
 #include <lib/address_resolve/AddressResolve.h>
+#include <lib/core/StringBuilderAdapters.h>
+#include <pw_unit_test/framework.h>
+#include <app/data-model/Nullable.h>
 
 using namespace chip;
 
-TEST(CASESessionManagerTest, InitFunctionExistsAndCanBeCalled)
+// Minimal stub classes to allow construction and Init
+namespace {
+class DummyReliableMessageMgr : public ReliableMessageMgr
 {
-    CASESessionManager manager;
+public:
+    void RegisterSessionUpdateDelegate(SessionUpdateDelegate *) override {}
+};
+
+class DummyExchangeMgr : public Messaging::ExchangeManager
+{
+public:
+    DummyReliableMessageMgr mReliableMessageMgr;
+    ReliableMessageMgr * GetReliableMessageMgr() override { return &mReliableMessageMgr; }
+};
+
+class DummySessionManager : public SessionManager
+{
+public:
+    Optional<SessionHandle> FindSecureSessionForNode(const ScopedNodeId &, Optional<Transport::SecureSession::Type>, TransportPayloadCapability) override
+    {
+        return NullOptional;
+    }
+};
+
+class DummyCASEClientPool : public CASEClientPoolDelegate
+{
+};
+
+class DummyOperationalSessionSetupPool : public OperationalSessionSetupPoolDelegate
+{
+public:
+    OperationalSessionSetup * Allocate(const CASEClientInitParams &, CASEClientPoolDelegate *, const ScopedNodeId &, OperationalSessionReleaseDelegate *) override { return nullptr; }
+    void Release(OperationalSessionSetup *) override {}
+    void ReleaseAllSessionSetupsForFabric(FabricIndex) override {}
+    void ReleaseAllSessionSetup() override {}
+    OperationalSessionSetup * FindSessionSetup(const ScopedNodeId &, bool) override { return nullptr; }
+};
+} // namespace
+
+TEST(CASESessionManagerTest, InitReturnsErrorOnInvalidParams)
+{
+    CASESessionManager mgr;
     CASESessionManagerConfig config;
-    chip::System::Layer * systemLayer = nullptr;
-    CHIP_ERROR err = manager.Init(systemLayer, config);
-    EXPECT_TRUE(err == CHIP_NO_ERROR || err != CHIP_NO_ERROR);
+    // sessionInitParams.Validate() will fail because exchangeMgr is nullptr
+    EXPECT_EQ(mgr.Init(nullptr, config), CHIP_ERROR_INVALID_ARGUMENT);
 }
 
-TEST(CASESessionManagerTest, ShutdownFunctionExistsAndCanBeCalled)
+TEST(CASESessionManagerTest, InitSucceedsWithValidParams)
 {
-    CASESessionManager manager;
-    manager.Shutdown();
-    EXPECT_TRUE(true);
+    CASESessionManager mgr;
+    CASESessionManagerConfig config;
+    DummyExchangeMgr dummyExchangeMgr;
+    DummySessionManager dummySessionManager;
+    config.sessionInitParams.exchangeMgr = &dummyExchangeMgr;
+    config.sessionInitParams.sessionManager = &dummySessionManager;
+    DummyCASEClientPool dummyClientPool;
+    DummyOperationalSessionSetupPool dummySessionSetupPool;
+    config.clientPool = &dummyClientPool;
+    config.sessionSetupPool = &dummySessionSetupPool;
+    EXPECT_EQ(mgr.Init(nullptr, config), CHIP_NO_ERROR);
 }
 
-TEST(CASESessionManagerTest, ReleaseAllSessionsFunctionExistsAndCanBeCalled)
+TEST(CASESessionManagerTest, ShutdownDoesNotCrash)
 {
-    CASESessionManager manager;
-    manager.ReleaseAllSessions();
-    EXPECT_TRUE(true);
-}
-
-TEST(CASESessionManagerTest, ReleaseSessionsForFabricFunctionExistsAndCanBeCalled)
-{
-    CASESessionManager manager;
-    FabricIndex fabricIndex = 1;
-    manager.ReleaseSessionsForFabric(fabricIndex);
-    EXPECT_TRUE(true);
-}
-
-TEST(CASESessionManagerTest, ReleaseSessionByPeerIdFunctionExistsAndCanBeCalled)
-{
-    CASESessionManager manager;
-    ScopedNodeId peerId;
-    manager.ReleaseSession(peerId);
-    EXPECT_TRUE(true);
-}
-
-TEST(CASESessionManagerTest, GetPeerAddressFunctionExistsAndCanBeCalled)
-{
-    CASESessionManager manager;
-    ScopedNodeId peerId;
-    Transport::PeerAddress addr;
-    CHIP_ERROR err = manager.GetPeerAddress(peerId, addr, TransportPayloadCapability::kMRPPayload);
-    EXPECT_TRUE(err == CHIP_NO_ERROR || err != CHIP_NO_ERROR);
-}
-
-TEST(CASESessionManagerTest, UpdatePeerAddressFunctionExistsAndCanBeCalled)
-{
-    CASESessionManager manager;
-    ScopedNodeId peerId;
-    manager.UpdatePeerAddress(peerId);
-    EXPECT_TRUE(true);
+    CASESessionManager mgr;
+    CASESessionManagerConfig config;
+    DummyExchangeMgr dummyExchangeMgr;
+    DummySessionManager dummySessionManager;
+    config.sessionInitParams.exchangeMgr = &dummyExchangeMgr;
+    config.sessionInitParams.sessionManager = &dummySessionManager;
+    DummyCASEClientPool dummyClientPool;
+    DummyOperationalSessionSetupPool dummySessionSetupPool;
+    config.clientPool = &dummyClientPool;
+    config.sessionSetupPool = &dummySessionSetupPool;
+    EXPECT_EQ(mgr.Init(nullptr, config), CHIP_NO_ERROR);
+    mgr.Shutdown();
 }
