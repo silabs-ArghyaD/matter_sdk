100881835dce43e6dd20e0e596a05a1ec1ca39d9
ardutta <arghya.dutta@silabs.com>
2025-10-07 16:22:58 +0530

    Added Coverage Report in Github Pages & added sl_build_unit_tests=true argument in build_coverage.sh

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -169,7 +169,7 @@ if [ "$skip_gn" == false ]; then
     fi
 
     # Generate ninja files
-    EXTRA_GN_ARGS=""
+    EXTRA_GN_ARGS="sl_build_unit_tests=true"
 
     # We only need 'chip_build_all_clusters_app' if we run YAML tests
     if [ "$ENABLE_YAML" == true ]; then
